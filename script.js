84e282be4b6183c38b7f8a22ca5f4bb558c80651
larissaborsari <larissa.borsari@acantolabs.com>
2023-11-20 10:04:31 -0700

    last commit

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-// generate a random number between 1 and 100 
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 100) + 1; // Generate a random number between 1 and 100
+};
 
+function checkGuess() {
+  var guess = parseInt(document.getElementById("txtInput").value);
+  var number = generateRandomNumber();
+  var attempts = 1; // Start attempts at 1
 
-//Prompt Guess so the user can guess the number
+  while (guess !== number) {
+    if (guess < number) {
+      guess = parseInt(prompt("Guess Higher: "));
+    } else if (guess > number) {
+      guess = parseInt(prompt("Guess Lower: "));
+    }
 
+    if (isNaN(guess)) {
+      // If input is not a number, ask again
+      guess = parseInt(prompt("Please enter a valid number: "));
+    }
 
-  //if wrong 
+    attempts++;
+  }
 
-    //guess higher
-
-    //guess lower
-
-
-  //if correct
+  if (guess === number) {
+    alert("Congratulations! You guessed the number in " + attempts + " attempt(s)");
+  }
+}
