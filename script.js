0facfcb101a15e4d4cb9fae3004c236147ad42d1
larissaborsari <larissa.borsari@acantolabs.com>
2023-11-20 09:50:08 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// generate a random number between 1 and 100 
+
+
+//Prompt Guess so the user can guess the number
+
+
+  //if wrong 
+
+    //guess higher
+
+    //guess lower
+
+
+  //if correct
